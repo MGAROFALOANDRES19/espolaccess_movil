569abb219e814f7d914a4026dc9c2600874c07d6
marco <mgarofalo.andres19@hotmail.com>
2018-11-21 15:13:27 -0500

    firstCommit

---
+++ app/src/main/java/com/access/espol/marco77713/espolaccess/MainActivity.java
@@ -0,0 +1,13 @@
+movipackage com.access.espol.marco77713.espolaccess;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
